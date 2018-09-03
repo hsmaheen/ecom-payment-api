377181d95b2bdb488c344a5444449ec3e5abcf08
Maheen <hsmaheen@gmail.com>
2018-09-03 20:24:16 +0800

    change db server

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     serverPort: process.env.PORT || 3200,
-    mongoDBUrl: process.env.MONGODB_URL || 'mongodb://127.0.0.1/local',
-    //mongoDBUrl: process.env.MONGODB_URL || 'mongodb+srv://hsmaheen:VxK40wZtx1MvyOoo@cluster0-nheri.gcp.mongodb.net/test?retryWrites=true'
+    // mongoDBUrl: process.env.MONGODB_URL || 'mongodb://127.0.0.1/local',
+    mongoDBUrl: process.env.MONGODB_URL || 'mongodb+srv://hsmaheen:VxK40wZtx1MvyOoo@cluster0-nheri.gcp.mongodb.net/test?retryWrites=true'
 };
